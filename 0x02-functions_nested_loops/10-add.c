a8016089480e1fadaa333607485248c74d41f763
Joshua-pelumi <joshuaogundairo9@gmail.com>
2022-09-14 16:45:49 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - adds two numbers
+ * @i: takes an integer variable
+ * @j: takes an integer variable
+ *  Return: result
+ */
+
+int add(int i, int j)
+{
+	int result = i + j;
+
+	return (result);
+}
