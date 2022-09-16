7c8d99750ae1114a4a1f95f27c742d2581033ab1
Joshua-pelumi <joshuaogundairo9@gmail.com>
2022-09-15 20:09:09 -0700

    Task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for(i = 0; i <= 100; i++)
+	{
+		if(i%15 == 0)
+			printf("FizzBuzz");
+		else if(i %5 ==0)
+			printf("Buzz");
+		else if(i%3 == 0)
+			printf("Fizz");
+		else 
+			printf("%d\n", i);	
+	}
+	return (0);
+}
