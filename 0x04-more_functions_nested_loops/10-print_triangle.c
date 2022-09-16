7aefd7f162645a0ed77ca136844e012dbb43c4a8
Joshua-pelumi <joshuaogundairo9@gmail.com>
2022-09-15 20:13:23 -0700

    Task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints triangle
+ * @size: size of the triange
+ *
+ */
+
+void print_triangle(int size)
+{
+	int i, j;
+
+	if (size > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for ((j = size - i); j > 0; j--)
+			{
+				_putchar(' ');
+			}
+			for (j = 0; j < i; j++)
+			{
+
+				_putchar('#');
+			}
+			if (i == size)
+			{
+				continue;
+			}
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
