82dc29674cd98495ec68f42c9882331740d441f9
Joshua-pelumi <joshuaogundairo9@gmail.com>
2022-09-22 14:55:16 -0700

    Task 0-3

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: first string
+ * @src: second string
+ *
+ * Return: a string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int dest_len = 0;
+
+	while (dest[i++])
+		dest_len++;
+
+	for (i = 0; src[i]; i++)
+		dest[dest_len++] = src[i];
+
+	return (dest);
+}
