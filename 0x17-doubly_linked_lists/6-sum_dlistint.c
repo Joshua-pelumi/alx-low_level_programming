54dd3c23c70cc50109538762341187f435a4e8a7
Joshua-pelumi <joshuaogundairo9@gmail.com>
2022-12-09 20:23:53 -0800

    Task on doubly-linked list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns the sum of all the data (n) of a dlistint_t list
+ * @head: pointer to head of the list
+ * Return: sum of all nodes or 0
+ **/
+int sum_dlistint(dlistint_t *head)
+{
+	long int sum;
+	dlistint_t *tmp;
+
+	sum = 0;
+	if (head == NULL)
+	return (sum);
+
+	tmp = head;
+	while (tmp)
+	{
+	sum += tmp->n;
+	tmp = tmp->next;
+	}
+	return (sum);
+}
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+
